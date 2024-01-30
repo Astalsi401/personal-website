302d998025669d9992ed57282c77f80d48485c52
Astalsi401 <Astalsi401@users.noreply.github.com>
2024-01-30 07:00:45 +0000

    deploy: 426ed8b790e55cefb3c3a20c39a8208ed6bb3835

--- assets/js/frame.js
+++ assets/js/frame.js
@@ -1,5 +1,12 @@
 window.addEventListener("click", () => parent.postMessage({ window: "iframe" }));
 const resizeObserver = new ResizeObserver((entries) => {
-  for (let entry of entries) parent.postMessage({ height: entry.contentRect.height + 16 });
+  const computedStyle = window.getComputedStyle ? getComputedStyle(document.body, null) : document.body.currentStyle;
+  let paddingMargin = 0;
+  for (let space of ["margin", "padding"]) {
+    for (let side of ["top", "bottom"]) {
+      paddingMargin += parseInt(computedStyle[`${space}-${side}`]);
+    }
+  }
+  for (let entry of entries) parent.postMessage({ height: entry.contentRect.height + paddingMargin });
 });
 resizeObserver.observe(document.body);
---
+++ assets/label-POkP99Db.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-ghIOUYFc.js";function j({label:e,name:n,type:a,step:r,min:i,max:t,placeholder:m,value:o,fuc:x}){return s.jsxs("label",{className:"my-2 w-100",children:[e&&s.jsxs("div",{children:[e,"："]}),s.jsx("input",{className:"p-1",name:n,type:a,step:r,min:i,max:t,placeholder:m,value:o,onChange:x})]})}export{j as L};
