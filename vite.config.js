20f54efe441c1b24efaa554267cbcdc3aa112329
Astalsi401 <misting401@gmail.com>
2024-01-31 09:20:49 +0800

    fix: refresh 404

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,6 @@ import { defineConfig } from "vite";
 import { VitePluginRadar } from "vite-plugin-radar";
 import react from "@vitejs/plugin-react-swc";
 
-const rewriteSlashToIndexHtml = () => {
-  return {
-    name: "rewrite-slash-to-index-html",
-    apply: "serve",
-    enforce: "post",
-    configureServer(server) {
-      // rewrite / as index.html
-      server.middlewares.use("/", (req, _, next) => {
-        if (req.url === "/") {
-          req.url = "/index.html";
-        }
-        next();
-      });
-    },
-  };
-};
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -29,8 +12,6 @@ export default defineConfig({
         id: "G-F8SLV521P4",
       },
     }),
-    rewriteSlashToIndexHtml(),
   ],
   base: "/personal-website",
-  appType: "mpa",
 });
