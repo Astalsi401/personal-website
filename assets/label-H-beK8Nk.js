c1dff8aaea6772713a11a73c3cda3fee10816236
Astalsi401 <Astalsi401@users.noreply.github.com>
2024-02-02 09:45:27 +0000

    deploy: d8f8105ffd314bc93265f77e69e253c89de2b2bc

--- assets/label-H-beK8Nk.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-t9TkrohJ.js";function j({label:e,name:n,type:a,step:r,min:i,max:t,placeholder:m,value:o,fuc:x}){return s.jsxs("label",{className:"my-2 w-100",children:[e&&s.jsxs("div",{children:[e,"："]}),s.jsx("input",{className:"p-1",name:n,type:a,step:r,min:i,max:t,placeholder:m,value:o,onChange:x})]})}export{j as L};
