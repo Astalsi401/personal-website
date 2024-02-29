a36b830861048dd0658cfac4946380db765d8781
Astalsi401 <misting401@gmail.com>
2024-02-29 10:18:52 +0800

    fix: codeChunk copy btn color

--- src/components/codeChunk.jsx
+++ src/components/codeChunk.jsx
@@ -37,7 +37,7 @@ export function CodeChunk({ code, lang, path }) {
   return (
     <pre className="my-2 p-2 pt-4 position-relative">
       {fileName && <span className="position-absolute ps-1 top-0 start-0 text-small text-gray">{fileName}</span>}
-      <a href="#" className={`copy-btn d-block position-absolute text-black ${isActive(active)}`} onClick={copy}>
+      <a href="#" className={`copy-btn d-block position-absolute text-code-text ${isActive(active)}`} onClick={copy}>
         <svg viewBox="0 0 10 10">
           <path fill="none" d="M4.5 1 L7.5 1 Q8.5 1,8.5 2 L8.5 6 Q8.5 7,7.5 7 L4.5 7 Q3.5 7,3.5 6 L3.5 2 Q3.5 1,4.5 1" />
           <path fill="none" d="M2.5 3 L5.5 3 Q6.5 3,6.5 4 L6.5 8 Q6.5 9,5.5 9 L2.5 9 Q1.5 9,1.5 8 L1.5 4 Q1.5 3,2.5 3" />
