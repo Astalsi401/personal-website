15887836ff4495463db754a6a2dfa928c510c9b3
Astalsi401 <misting401@gmail.com>
2024-03-21 09:30:39 +0800

    add: css auto height

---
+++ src/pages/portfolio/css-auto-height/page.jsx
@@ -0,0 +1,27 @@
+import { CodeChunk } from "../../../components/codeChunk";
+import { DemoFrame } from "../../../components/demoFrame";
+
+const demoPath = `${import.meta.env.BASE_URL}/assets/demo-files/portfolio/css-auto-height`;
+
+const sections = [
+  {
+    title: "",
+    content: (
+      <>
+        <div className="my-2">
+          資料來源：
+          <a href="https://youtu.be/B_n4YONte5A?si=D5iH7jq3Em-8c8sz" target="_blank" rel="noopener noreferrer">
+            The simple trick to transition from height 0 to auto with CSS - By Kevin Powell
+          </a>
+        </div>
+        <div className="my-2">
+          <code>height: auto;</code>無法使用transition製作動畫效果，然而可以將<code>grid-template-rows</code>修改為<code>0fr</code>、<code>1fr</code>來達成auto hight transition
+        </div>
+        <DemoFrame src={`${demoPath}/frame1.html`} />
+        <CodeChunk path={`${demoPath}/css-auto-height.scss`} lang="scss" />
+        <CodeChunk path={`${demoPath}/frame1/css-auto-height.html`} lang="html" />
+      </>
+    ),
+  },
+];
+export default sections;
