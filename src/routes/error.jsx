57e7fef798401852778854a8d974c188bd52a97f
Astalsi401 <misting401@gmail.com>
2024-04-30 10:53:52 +0800

    update: error page

--- src/routes/error.jsx
+++ src/routes/error.jsx
@@ -1,16 +1,13 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
-
   return (
     <div id="error-page text-center">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+      <Link to={import.meta.env.BASE_URL}>Back to Home Page</Link>
     </div>
   );
 }
