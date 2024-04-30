2672d00bd558e5d9e82e2c9943b8672a595b61c9
Astalsi401 <misting401@gmail.com>
2024-04-30 15:43:33 +0800

    fix: aside-content last did not hl

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useLoaderData, useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { updateStore } from "../assets/store";
@@ -15,6 +15,14 @@ export function Sidebar({ wrapperRef }) {
   const { page, href } = useParams();
   const click = () => setChildrenActive((prev) => !prev);
   const handleLinkClick = () => dispatch(updateStore({ sidebarActive: false }));
+  const handleScrollEnd = () => {
+    const { scrollY, innerHeight } = window;
+    if (scrollY + innerHeight >= document.body.scrollHeight) dispatch(updateStore({ currentPostTitles: currentPostTitles.map((s, i) => ({ title: s.title, active: i === currentPostTitles.length - 1 })) }));
+  };
+  useEffect(() => {
+    document.addEventListener("scroll", handleScrollEnd);
+    return () => document.removeEventListener("scroll", handleScrollEnd);
+  }, []);
   return (
     <aside id={sidebarID} className={`position-absolute ${isActive(sidebarActive)}`} ref={wrapperRef}>
       <h1 className="pt-5 pb-3 text-center">
