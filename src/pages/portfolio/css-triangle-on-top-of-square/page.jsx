40a567b06fa6efd918624a57456f4cde6877536f
Astalsi401 <misting401@gmail.com>
2024-06-21 17:57:22 +0800

    add: portfolio so answer triangle on square

---
+++ src/pages/portfolio/css-triangle-on-top-of-square/page.jsx
@@ -0,0 +1,27 @@
+import { CodeChunk } from "../../../components/codeChunk";
+import { DemoFrame } from "../../../components/demoFrame";
+
+const Sections = (demoPath) => [
+  {
+    title: "How to create a triangle on the top of a square?",
+    content: (
+      <>
+        OP links:
+        <a href="https://stackoverflow.com/q/78650084" target="_blank">
+          https://stackoverflow.com/q/78650084
+        </a>
+      </>
+    ),
+  },
+  {
+    title: "My solution",
+    content: (
+      <>
+        <DemoFrame src={`${demoPath}/frame1/frame.html`} />
+        <CodeChunk path={`${demoPath}/frame1/main.css`} lang="css" />
+        <CodeChunk path={`${demoPath}/frame1/main.html`} lang="html" />
+      </>
+    ),
+  },
+];
+export default Sections;
