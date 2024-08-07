f1ccbcc8afd4c60c3a056a66b8c64df0d9785c59
Astalsi401 <misting401@gmail.com>
2024-08-07 14:09:11 +0800

    update: lower density

--- public/assets/demo-files/portfolio/nest-animate/frame1/nest-animate.js
+++ public/assets/demo-files/portfolio/nest-animate/frame1/nest-animate.js
@@ -1,6 +1,6 @@
 class NestAnimate {
   constructor(target) {
-    this.setting = { zIndex: -1, opacity: 0.5, color: "0,0,0", count: 90, density: 0.0003 };
+    this.setting = { zIndex: -1, opacity: 0.5, color: "0,0,0", count: 90, density: 0.0002 };
     this.target = target;
     this.canvas = document.createElement("canvas");
     this.target.appendChild(this.canvas);
