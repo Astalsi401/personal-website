ea3d0caac9669ae9f571a5ef440db151beb661fd
Astalsi401 <misting401@gmail.com>
2024-08-12 11:52:43 +0800

    update: loading ts

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,14 +1,14 @@
 import { useNavigation } from "react-router-dom";
 
-export const Loading = ({ children }) => {
+export const Loading: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const { state } = useNavigation();
   return state === "loading" ? <LoadingAnimation /> : children;
 };
 
-export const LoadingAnimation = () => (
+export const LoadingAnimation: React.FC = () => (
   <div className="loading">
     {Array.from({ length: 24 }).map((_, i) => (
-      <div key={`loading-line-${i}`} className="line" style={{ "--i": i }} />
+      <div key={`loading-line-${i}`} className="line" style={{ "--i": i } as React.CSSProperties} />
     ))}
   </div>
 );
