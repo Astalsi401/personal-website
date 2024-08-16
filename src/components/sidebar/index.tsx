4ee4c1ac452cae3e78228e60df7687c83c6117ea
Astalsi401 <misting401@gmail.com>
2024-08-16 15:35:24 +0800

    update: delete unnesessary scss

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -17,7 +17,10 @@ export const Sidebar: React.FC<SidebarProps> = ({ wrapperRef }) => {
   const handleLinkClick = () => dispatch(updateStore({ sidebarActive: false }));
   return (
     <aside id={sidebarID} className={`position-fixed ${isActive(sidebarActive)}`} ref={wrapperRef}>
-      <h1 className="pt-5 pb-3 text-center">
+      <div className="py-5 d-flex justify-content-center align-items-center">
+        <a className="social-link d-block" href="https://github.com/Astalsi401" target="_blank" title="My GitHub" style={{ mask: `url(${import.meta.env.BASE_URL}/assets/images/github-mark.svg)`, maskSize: "cover" }}></a>
+      </div>
+      <h1 className="px-2">
         <Link to={`${import.meta.env.BASE_URL}${index.href}`} id={sidebarAnchorID} className="text-decoration-none" onClick={handleLinkClick}>
           {index.category}
         </Link>
@@ -29,7 +32,7 @@ export const Sidebar: React.FC<SidebarProps> = ({ wrapperRef }) => {
           const showChildren = isCurrent && currentPostTitles.length > 0;
           return (
             <li key={i} className={`${showChildren ? "has-children" : ""}`} onClick={click}>
-              <Link to={relativePath ? `${import.meta.env.BASE_URL}${index.href === "/" ? "" : index.href}${p.href}` : p.href} target={relativePath ? "_self" : "_blank"} className={`px-3 text-decoration-none text-large text-bold ${isCurrent ? "current" : ""}`} onClick={handleLinkClick}>
+              <Link to={relativePath ? `${import.meta.env.BASE_URL}${index.href === "/" ? "" : index.href}${p.href}` : p.href} target={relativePath ? "_self" : "_blank"} className={`px-3 py-2 d-block text-decoration-none text-large text-bold ${isCurrent ? "current" : ""}`} onClick={handleLinkClick}>
                 <span>{p.page}</span>
               </Link>
               {showChildren && <SidebarChild currentPostTitles={currentPostTitles} childrenActive={childrenActive} />}
@@ -37,9 +40,6 @@ export const Sidebar: React.FC<SidebarProps> = ({ wrapperRef }) => {
           );
         })}
       </ul>
-      <div className="pt-3 pb-5 d-flex justify-content-center align-items-center">
-        <a className="social-link d-block" href="https://github.com/Astalsi401" target="_blank" title="My GitHub" style={{ mask: `url(${import.meta.env.BASE_URL}/assets/images/github-mark.svg)`, maskSize: "cover" }}></a>
-      </div>
     </aside>
   );
 };
