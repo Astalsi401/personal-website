d0f82a7fbf5c5f481badb32b00329602b26b13b8
Astalsi401 <misting401@gmail.com>
2024-12-05 09:14:49 +0800

    add: npm notes

---
+++ src/pages/notes/npm/page.tsx
@@ -0,0 +1,28 @@
+import { CodeChunk } from "@components";
+import type { SectionsProps } from "@types";
+
+const Sections: SectionsProps = () => [
+  {
+    title: "packages management",
+    content: (
+      <>
+        <ul>
+          <li>
+            檢查可更新的packages
+            <CodeChunk code={`npm outdated`} lang="bash" />
+          </li>
+          <li>
+            有限制的更新(不跨主版號)
+            <CodeChunk code={`npm update`} lang="bash" />
+          </li>
+          <li>
+            列出當前packages的版本
+            <CodeChunk code={`npm list`} lang="bash" />
+          </li>
+        </ul>
+      </>
+    ),
+  },
+];
+
+export default Sections;
