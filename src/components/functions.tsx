e5ac23c4db65ecd7e2bc4071edf2a71dc68a095d
Astalsi401 <misting401@gmail.com>
2025-04-30 09:57:52 +0800

    update: simplify

--- src/components/functions.tsx
+++ src/components/functions.tsx
@@ -7,7 +7,7 @@ export const titleToHash = (title: string) => title.replace(" ", "_");
 export const getIndex: () => Promise<IndexJson> = async () => await fetch(`${import.meta.env.BASE_URL}/assets/json/index.json`).then((res) => res.json());
 export const getCategories: () => Promise<Categories> = async () => {
   const { index, root, href } = await getIndex();
-  return { category: root, href: href, pages: index.map((d) => ({ page: d.category, href: d.href, icon: d.icon })) };
+  return { category: root, href, pages: index.map((d) => ({ page: d.category, href: d.href, icon: d.icon })) };
 };
 export const getPages = async ({ params: { href } }: { params: Params }) => {
   const { index } = await getIndex();
