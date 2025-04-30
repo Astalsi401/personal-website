40f177d052754ff082eb45bc66eb2afc7676d43c
Astalsi401 <misting401@gmail.com>
2025-04-30 11:20:51 +0800

    update: del unnecessary function

--- src/components/demo-frame/index.tsx
+++ src/components/demo-frame/index.tsx
@@ -6,25 +6,25 @@ export const DemoFrame: React.FC<DemoFrameProps> = ({ src, html, cssHref, js, li
   const iframeRef = useRef<null | HTMLIFrameElement>(null);
   const [height, setHeight] = useState<number>(0);
   const [fullPage, setFullPage] = useState<boolean>(false);
-  const handleMessage = ({ data, source }: MessageEvent) => source === iframeRef.current?.contentWindow && data.height && setHeight(data.height);
-  const iFrameLoad = ({ data, source }: MessageEvent) => !src && source === iframeRef.current?.contentWindow && data.load && iframeRef.current.contentWindow?.postMessage({ html, cssHref, js });
+  const handleMessage = ({ data, source }: MessageEvent) => {
+    if (source !== iframeRef.current?.contentWindow) return;
+    data.height && setHeight(data.height);
+    !src && data.load && iframeRef.current.contentWindow?.postMessage({ html, cssHref, js });
+  };
   const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
     setFullPage((prev) => {
-      const newState = !prev;
-      document.body.style.overflowY = newState ? "hidden" : "auto";
-      return newState;
+      document.body.style.overflowY = !prev ? "hidden" : "auto";
+      return !prev;
     });
   };
   useEffect(() => {
     const controller = new AbortController();
-    const signal = controller.signal;
-    window.addEventListener("message", iFrameLoad, { signal });
-    window.addEventListener("message", handleMessage, { signal });
+    window.addEventListener("message", handleMessage, { signal: controller.signal });
     return () => controller.abort();
   }, []);
   return (
-    <div className="demo-frame my-2 p-2 pt-0">
+    <div className="demo-frame my-2 p-2 pt-0 position-relative">
       <a href="#" className={clsx("full-page mb-1 ps-2 pe-4 d-block position-relative float-end text-small text-primary", isActive(fullPage))} onClick={handleClick}>
         {fullPage ? "Close" : "Full Page"}
       </a>
