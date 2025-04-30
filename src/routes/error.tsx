bd18eecfd64c47c7c75d00d4728f0cb41eb225a9
Astalsi401 <misting401@gmail.com>
2025-04-30 18:10:50 +0800

    update: error page styles

--- src/routes/error.tsx
+++ src/routes/error.tsx
@@ -4,10 +4,12 @@ export const ErrorPage: React.FC = () => {
   const error = useRouteError() as Error;
   console.error(error);
   return (
-    <div id="error-page text-center">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <Link to={import.meta.env.BASE_URL}>Back to Home Page</Link>
+    <div className="d-flex justify-content-center align-items-center" style={{ minHeight: "100vh" }}>
+      <div className="text-center">
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <Link to={import.meta.env.BASE_URL}>Back to Home Page</Link>
+      </div>
     </div>
   );
 };
