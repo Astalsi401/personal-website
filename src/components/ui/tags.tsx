ceee5effb9848b16621dcc6cab671f569ef48244
Astalsi401 <misting401@gmail.com>
2025-05-02 13:58:44 +0800

    add: Tag component

--- src/components/ui/tags.tsx
+++ src/components/ui/tags.tsx
@@ -3,9 +3,15 @@ import { clsx } from "@functions";
 export const Tags: React.FC<{ className?: string; tags: string[] }> = ({ className, tags }) => (
   <div className={clsx("page-tags p-1 d-flex flex-wrap align-items-end", className)}>
     {tags.map((tag, i) => (
-      <div key={tag} className="page-tag bg-primary text-white text-small px-1 m-1 rounded-1 shadow-sm" style={{ "--i": i } as React.CSSProperties}>
+      <Tag key={tag} className="bg-primary text-white text-small" style={{ "--i": i } as React.CSSProperties}>
         {tag}
-      </div>
+      </Tag>
     ))}
+  </div>
+);
+
+export const Tag: React.FC<{ children: React.ReactNode; className?: string; style?: React.CSSProperties }> = ({ children, className, style }) => (
+  <div className={clsx("page-tag px-1 m-1 rounded-1 shadow-sm", className)} style={style}>
+    {children}
   </div>
 );
