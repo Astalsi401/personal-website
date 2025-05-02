df320e0176181d2778e3257b9ab4dea58a86dd19
Astalsi401 <misting401@gmail.com>
2025-05-02 14:07:14 +0800

    fixL vscode styles

--- src/components/ui/kbd.tsx
+++ src/components/ui/kbd.tsx
@@ -1,1 +1,1 @@
-export const Kbd: React.FC<{ children: string }> = ({ children }) => <kbd className="d-inlin-block rounded-1 py-0 px-1 m-1 bg-black-80">{children}</kbd>;
+export const Kbd: React.FC<{ children: React.ReactNode }> = ({ children }) => <kbd className="d-inlin-block rounded-1 py-0 px-1 m-1 bg-black-80">{children}</kbd>;
