778afe901a488736086108add4268e8ad29b321d
Astalsi401 <misting401@gmail.com>
2025-05-05 15:27:55 +0800

    update: ref in Input component

--- src/components/ui/form/label.tsx
+++ src/components/ui/form/label.tsx
@@ -2,9 +2,10 @@ import { clsx } from "@functions";
 import { InputHTMLAttributes } from "react";
 
 interface InputProps extends React.DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
+  elemRef?: React.RefObject<HTMLInputElement>;
   placeholder?: string;
 }
-export const Input: React.FC<InputProps> = ({ className, ...props }) => <input className={clsx("p-1 shadow-1 border-0", className)} {...props} />;
+export const Input: React.FC<InputProps> = ({ className, elemRef, ...props }) => <input ref={elemRef} className={clsx("p-1 shadow-1 border-0", className)} {...props} />;
 
 type LabelProps = { label?: string } & InputProps;
 export const Label: React.FC<LabelProps> = ({ label, ...props }) => (
