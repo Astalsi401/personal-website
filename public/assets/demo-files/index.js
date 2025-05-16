dea086620fb9f361c5c50c185749762691aa4725
Astalsi401 <misting401@gmail.com>
2025-05-16 21:39:03 +0800

    fix: default iframe height

--- public/assets/demo-files/index.js
+++ public/assets/demo-files/index.js
@@ -3,15 +3,16 @@ const insertHtml = async (html) => {
     .then((res) => (res.ok ? res.text() : ""))
     .catch((err) => err.message.includes("404") && "");
 };
-const insertCss = async (css) => {
-  document.head.querySelector(`style[data-demo="style"]`)?.remove();
-  document.head.innerHTML += `<style data-demo="style">${css}</style>`;
+const insertCss = async (css, value) => {
+  document.head.querySelector(`style[data-demo="${value}"]`)?.remove();
+  document.head.innerHTML += `<style data-demo="${value}">${css}</style>`;
 };
 const demoContent = async ({ html, css, js }) => {
   try {
     html && (await insertHtml(html));
-    css && (await insertCss(css));
+    css && (await insertCss(css, "main"));
     js && (await Promise.all(js.map((lib) => import(lib))));
+    await insertCss("body{min-height:200px;}", "default");
   } catch (error) {
     console.error(error);
   }
