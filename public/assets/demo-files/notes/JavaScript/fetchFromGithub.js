b2cc6b9e701a823e4cb37f2f723e9d805f238e58
Astalsi401 <misting401@gmail.com>
2025-05-20 17:48:25 +0800

    update: move javascript page from notes to portfolio

--- public/assets/demo-files/notes/JavaScript/fetchFromGithub.js
+++
@@ -1,8 +0,0 @@
-const getgit = async (owner, repo, path) => {
-  const { sha } = await fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`).then((res) => res.json());
-  const { content } = await fetch(`https://api.github.com/repos/${owner}/${repo}/git/blobs/${sha}`).then((blob) => blob.json());
-  const data = await JSON.parse(atob(content));
-  console.log(data);
-};
-// the repo "ibmiWork" has been deleted
-getgit("Astalsi401", "ibmiWork", "test/test.json"); // output: { a: 0, b: [ 'a', 'b' ] }
--- src/pages/portfolio/github-api/page.tsx
+++ src/pages/portfolio/github-api/page.tsx
@@ -3,10 +3,10 @@ import type { SectionsProps } from "@/types";
 
 export const Sections: SectionsProps = ({ demoPath }) => [
   {
-    title: "fetch獲取儲存於github的檔案",
+    title: "fetch 獲取儲存於 github 的檔案",
     content: (
       <>
-        <CodeChunk path={`${demoPath}/fetchFromGithub.js`} lang="js" />
+        <CodeChunk path={`${demoPath}/fetchFromGithub.ts`} lang="ts" />
         原文：
         <a href="https://betterprogramming.pub/how-to-fetch-files-from-github-in-javascript-e0ed2c72aeb4" target="_blank">
           How to Fetch Files From GitHub in JavaScript
