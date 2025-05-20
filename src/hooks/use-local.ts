051f6ab750d65a714b427d7e8239be238ed240da
Astalsi401 <misting401@gmail.com>
2025-05-20 16:29:33 +0800

    add: expired time for localstorage cache

--- src/hooks/use-local.ts
+++ src/hooks/use-local.ts
@@ -1,8 +1,22 @@
-export const useLocal = (key: string) => {
-  const setLocal = (value: any) => localStorage.setItem(key, JSON.stringify(value));
+type LocalDiapatchOptions = { expired: number /* seconds */ };
+type LocalDiapatch = (value: any, options?: LocalDiapatchOptions) => void;
+
+export const useLocal = (key: string): [any, LocalDiapatch] => {
+  const setLocal = (value: any, options?: LocalDiapatchOptions) => {
+    if (key.length === 0) return;
+    localStorage.setItem(
+      key,
+      JSON.stringify({
+        value,
+        expired: options?.expired ? new Date().getTime() + options.expired * 1000 : null,
+      })
+    );
+  };
   const getLocal = () => {
-    const value = localStorage.getItem(key);
-    return value ? JSON.parse(value) : null;
+    const data = localStorage.getItem(key);
+    if (data === null || key.length === 0) return null;
+    const { value, expired } = JSON.parse(data);
+    return expired && new Date().getTime() > expired ? null : value;
   };
   return [getLocal(), setLocal];
 };
