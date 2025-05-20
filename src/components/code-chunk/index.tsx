fb58c5c950ce772da9e9ac7c8fa4c3f416e3c7d1
Astalsi401 <misting401@gmail.com>
2025-05-20 17:47:46 +0800

    update: gitApi()

--- src/components/code-chunk/index.tsx
+++ src/components/code-chunk/index.tsx
@@ -28,11 +28,11 @@ type CodeChunkProps = { code?: string; lang?: string; path?: string };
 
 const isGitHubPath = (path: string) => path.startsWith("https://github.com/");
 const gitApi = async (url: string, signal: AbortSignal) => {
-  const info = url.split("/");
-  const [owner, repo] = info.slice(3, 5);
-  const path = info.slice(7, info.length).join("/");
-  const { sha } = await fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`, { signal }).then((res) => res.json());
-  const { content } = await fetch(`https://api.github.com/repos/${owner}/${repo}/git/blobs/${sha}`, { signal }).then((blob) => blob.json());
+  const [, , , owner, repo, , , ...pathStr] = url.split("/");
+  const path = pathStr.join("/");
+  const repoUrl = `https://api.github.com/repos/${owner}/${repo}`;
+  const { sha } = await fetch(`${repoUrl}/contents/${path}`, { signal }).then((res) => res.json());
+  const { content } = await fetch(`${repoUrl}/git/blobs/${sha}`, { signal }).then((blob) => blob.json());
   return atob(content);
 };
 const normalFetch = async (path: string, signal: AbortSignal) =>
