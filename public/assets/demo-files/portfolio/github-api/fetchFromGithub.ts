9b7cf4937a18d20398901b0145d2fbdca171d015
Astalsi401 <misting401@gmail.com>
2025-05-21 11:47:08 +0800

    update: simplified example code

--- public/assets/demo-files/portfolio/github-api/fetchFromGithub.ts
+++ public/assets/demo-files/portfolio/github-api/fetchFromGithub.ts
@@ -1,7 +1,6 @@
 const gitApi = async (url: string, signal: AbortSignal) => {
-  const info = url.split("/");
-  const [owner, repo] = info.slice(3, 5);
-  const path = info.slice(7, info.length).join("/");
+  const [, , , owner, repo, , , ...pathStr] = url.split("/");
+  const path = pathStr.join("/");
   const { sha } = await fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`, { signal }).then((res) => res.json());
   const { content } = await fetch(`https://api.github.com/repos/${owner}/${repo}/git/blobs/${sha}`, { signal }).then((blob) => blob.json());
   return atob(content);
