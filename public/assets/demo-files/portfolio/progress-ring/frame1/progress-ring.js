5360a0df2c090b971a2c39f0d2bea9e0dfb27eeb
Astalsi401 <misting401@gmail.com>
2025-06-03 14:28:04 +0800

    fix: progress ring demo

--- public/assets/demo-files/portfolio/progress-ring/frame1/progress-ring.js
+++ public/assets/demo-files/portfolio/progress-ring/frame1/progress-ring.js
@@ -6,7 +6,7 @@ function getScrollPercent() {
 function update_scroll() {
   const scrollPercent = getScrollPercent();
   document.getElementById("scroll-line").style.setProperty("--scrollPercent", scrollPercent);
-  document.getElementById("progress").textContent = scrollPercent ? Math.round(scrollPercent * 100) : 0 + "%";
+  document.getElementById("progress").textContent = scrollPercent ? Math.round(scrollPercent * 100) : 0;
 }
 
 window.addEventListener("scroll", update_scroll);
