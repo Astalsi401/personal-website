978112f4651a2a65395e5c82981dac3f8756b14b
Astalsi401 <misting401@gmail.com>
2025-06-11 23:27:22 +0800

    fix: heatmap demo source path

--- src/pages/portfolio/d3js-heatmap/page.tsx
+++ src/pages/portfolio/d3js-heatmap/page.tsx
@@ -6,7 +6,12 @@ export const Sections: SectionsProps = ({ demoPath }) => [
     title: "",
     content: (
       <>
-        <DemoFrame html={`${demoPath}/frame1/heatmap.html`} scssHref={`${demoPath}/frame1/heatmap.css`} js={[`${demoPath}/frame1/heatmap.js`]} lib="d3js" />
+        <DemoFrame
+          html={`${demoPath}/frame1/heatmap.html`}
+          scssHref={`${demoPath}/frame1/heatmap.scss`}
+          js={[`${demoPath}/frame1/heatmap.js`]}
+          lib="d3js"
+        />
         <CodeChunk path={`${demoPath}/frame1/heatmap.js`} lang="js" />
         <CodeChunk path={`${demoPath}/frame1/heatmap.scss`} lang="scss" />
         <CodeChunk path={`${demoPath}/frame1/heatmap.html`} lang="html" />
